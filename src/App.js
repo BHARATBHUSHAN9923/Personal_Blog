89efab75d1fe1dc1dce8b4e0ba6b499dc9482081
BHARATBHUSHAN9923 <bharatbh@iitg.ac.in>
2021-09-08 19:14:39 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './Componenets/Navbar';
+import Home from './Componenets/pages/Home'
+import Services from './Componenets/pages/Services'
+import ACV from './Componenets/pages/ACV'
+import SignUp from './Componenets/pages/SignUp'
+import Footer from './Componenets/Footer';
+import Post from './Componenets/pages/Post';
+import Project from './Componenets/pages/Project';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      
+      <Switch>
+        <Route path = '/' exact component = {Home}/>
+        <Route path = '/project' exact component = {Project}/>
+        <Route path = '/ACV' exact component = {ACV}/>
+        <Route path='/sign-up' component={() => { 
+          window.location.href = 'https://www.youtube.com/channel/UCaqnSwav0t1NwhEUDniBgqg'; 
+          return null;
+      }}/>
+       <Route path='/instagram' component={() => { 
+          window.location.href = 'https://www.instagram.com/mrmeanor'; 
+          return null;
+      }}/>
+       <Route path='/facebook' component={() => { 
+          window.location.href = 'https://www.facebook.com/bharatbhushan.karhade'; 
+          return null;
+      }}/>
+       <Route path='/twitter' component={() => { 
+          window.location.href = 'https://www.twitter.com/bharatkarhade'; 
+          return null;
+      }}/>
+        <Route path='/linkedin' component={() => { 
+          window.location.href = 'https://www.linkedin.com/in/bharatbh'; 
+          return null;
+      }}/>
+        <Route path = '/post' exact component = {Post}/>
+        
+      </Switch>
+      <Footer/>
+    </Router>
+      
+    </>
   );
 }
 
---
+++ src/Componenets/Footer.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './Footer.css';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+        You can always contact me on social media or my personal G-Mail
+        </p>
+        <p className='footer-subscription-text'>
+            bharatekarhade@gmail.com
+        </p>
+      </section>
+      <section class='social-media'>
+        <div class='social-media-wrap'>
+          <div class='footer-logo'>
+            <Link to='/' className='social-logo'>
+            <i class="fas fa-code"></i>
+            <p> code/game </p>
+            <i class="fas fa-gamepad"></i>
+            </Link>
+          </div>
+          <div class='social-icons'>
+            <Link
+              class='social-icon-link facebook'
+              to='/facebook'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <i class='fab fa-facebook-f' />
+            </Link>
+            <Link
+              class='social-icon-link instagram'
+              to='/instagram'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <i class='fab fa-instagram' />
+            </Link>
+            <Link
+              class='social-icon-link youtube'
+              to='/sign-up'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <i class='fab fa-youtube' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/twitter'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <i class='fab fa-twitter' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/linkedin'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <i class='fab fa-linkedin' />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
